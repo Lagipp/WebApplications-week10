5b0f79b9c86064787492401ebf1838edf5cc0b3b
Miika Pynttäri <miika.pynttari@gmail2.com>
2021-11-23 15:12:14 +0200

    task 1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import About from "./components/About"
+import MyContainer from './components/MyContainer';
+import Header from './components/Header';
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<> <Header /> <MyContainer /> </>}/>
+          <Route path="/about" element={<> <Header /> <About /> </>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
