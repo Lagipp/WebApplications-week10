e747d2b35dc9df5923a3e418a881a79074631e5a
Miika Pynttäri <miika.pynttari@gmail2.com>
2021-11-24 16:54:32 +0200

    task 4 halfway done

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,11 @@ import '@fontsource/roboto/700.css';
 
 import { Link as RouterLink } from 'react-router-dom'
 import Button from '@mui/material/Button'
+
+import { useTranslation } from 'react-i18next';
+import { Suspense } from 'react';
+
+
 //import MenuIcon from '@mui/icons-material/Menu';
 
 
@@ -31,28 +36,45 @@ const buttonStyle = {
 
 
 function Header() {
+
+    const { t, i18n } = useTranslation();
+    const changeLanguage = (language) => {
+        i18n.changeLanguage(language)
+    }
+
     return (
+
+        <Suspense fallback={"PAGE IS LOADING..."}>
+
         <div>
+
             <h1> Header </h1>
 
-            <Box sx={{ flexGrow: 1 }}>
+            {/*<Box sx={{ flexGrow: 1 }}>*/}
             <AppBar position="static">
                 <Toolbar variant="dense">
-                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                
-                </IconButton>
-                <Typography variant="h6" color="inherit" component="div">
+                    <Box sx={{ flexGrow: 1 }}>
+                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    
+                    </IconButton>
+                    <Typography variant="h6" color="inherit" component="div">
 
-                    <Button style={buttonStyle} component={RouterLink} to="/"> Home </Button>
-                    <Button style={buttonStyle} component={RouterLink} to="/about">About</Button>
+                        <Button style={buttonStyle} component={RouterLink} to="/"> {t('Home')} </Button>
+                        <Button style={buttonStyle} component={RouterLink} to="/about"> {t('About')} </Button>
 
+                    </Typography>
+                    </Box>
 
-                </Typography>
+                        <Button style={buttonStyle} onClick={()=> changeLanguage("fi")}> FI </Button>
+                        <Button style={buttonStyle} onClick={()=> changeLanguage("en")}> EN </Button>
+
                 </Toolbar>
             </AppBar>
-            </Box>
+            {/*</Box>*/}
 
         </div>
+
+        </Suspense>
     )
 }
 
