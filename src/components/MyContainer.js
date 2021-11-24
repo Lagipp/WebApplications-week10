01a399455b4b43e8ebfdd5f3a797c3def96e9569
Miika Pynttäri <miika.pynttari@gmail2.com>
2021-11-24 19:24:13 +0200

    started task5

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { useTranslation } from 'react-i18next';
 import { Suspense } from 'react';
 
+import MyHOC from './MyHOC';
 
 
 
@@ -10,10 +11,20 @@ function MyContainer() {
 
     const { t, i18n } = useTranslation();
 
+    const Component = ({ name }) => {
+        return <div> Hello { name }! </div>;
+    }
+
+
+
+    //const wrappedWithName = MyHOC(Component, { name: "Miika"} );
+
     return (
         <div>
             <h2> MyContainer </h2>
             <p> {t('This is the front page')} </p>
+            {/*<p> { MyHOC(Component, { name: "Miika"} ) } </p>*/}
+            
         </div>
     )
 }
--- src/components/MyHOC.js
+++ src/components/MyHOC.js
@@ -1,20 +1,8 @@
 
 
-
-
-const Component = ({ name }) => {
-    return <div> Hello { name }! </div>;
-}
-
-
-
 const MyHOC = (oldComponent, props) => {
-    return <div className="wrapper"> oldComponent({...props}) </div>
+    return <div className="wrapper"> oldComponent({props}) </div>
 }
-
-
-
-const wrappedWithName = MyHOC(Component, { name: "Miika"} );
 
 
 export default MyHOC
