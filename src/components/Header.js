a7620e68f30257ef118fda61e3feb0f94f9fbb36
Miika Pynttäri <miika.pynttari@gmail2.com>
2021-11-24 16:14:33 +0200

    task 3 done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import { AppBar } from '@mui/material'
+import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { Link as RouterLink } from 'react-router-dom'
+import Button from '@mui/material/Button'
+//import MenuIcon from '@mui/icons-material/Menu';
+
+
+
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+/>
+
+
+/* https://stackoverflow.com/questions/42156150/how-to-add-multiple-buttons-in-appbar-using-iconelementright*/ 
+/* the solution for invisible buttons */
+
+const buttonStyle = {
+    color: 'white'
+}
 
 
 function Header() {
@@ -15,23 +37,21 @@ function Header() {
 
             <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
+                <Toolbar variant="dense">
+                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                
+                </IconButton>
+                <Typography variant="h6" color="inherit" component="div">
+
+                    <Button style={buttonStyle} component={RouterLink} to="/"> Home </Button>
+                    <Button style={buttonStyle} component={RouterLink} to="/about">About</Button>
+
+
+                </Typography>
                 </Toolbar>
             </AppBar>
             </Box>
+
         </div>
     )
 }
