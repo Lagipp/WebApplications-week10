d2b8e7d76255262f4bd2920f79ad78a859b6a2a7
Miika Pynttäri <miika.pynttari@gmail2.com>
2021-11-24 13:13:44 +0200

    task 2 done

--- src/components/About.js
+++ src/components/About.js
@@ -3,34 +3,60 @@ import { useState, useEffect } from 'react'
 
 function About() {
 
-    const [title, setTitle] = useState("")
-    const [key, setKey] = useState("")
+    const [title, setTitle] = useState(null)
+    //const [key, setKey] = useState("")
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
         .then(result => {
 
-            setTitle(JSON.stringify(result.title))
+            setTitle((result))
             //setTitle(JSON.stringify(result, ['title']));
             //setKey(JSON.stringify(result, ['id']));
 
 
-            //console.log(result);
-            //console.log(`the value of title is ${title}`);
-            //console.log(`the value of key is ${key}`);
+            /* console.log(`the value of result is ${JSON.stringify(result)}`);
+            console.log(`the value of title is ${JSON.stringify(title)}`);
+
+            console.log(`the typeof 'result' is ${typeof(result)}`)
+            console.log(`the typeof 'title' is ${typeof(title)}`) */
 
         }
     )}, [])
+
+    //console.log(`the value of result is ${JSON.stringify(result)}`);
+
+
+
+    //console.log(`the value of title is ${JSON.stringify(title)}`);
+
+    //console.log(`the typeof 'result' is ${typeof(result)}`)
+
+
+    //console.log(`the typeof 'title' is ${typeof(title)}`)
+
+
+    /* jesus this took me fckng forever to fix :(((( */
+    /* https://stackoverflow.com/questions/54609282/typeerror-cannot-read-property-map-of-null/54609332 */
+
+    let listItems = []
+
+    if (title)
+    {
+        listItems = title.map((listItem) => <li key={ listItem.id }> { listItem.title } </li>)
+    }
 
     return (
         <div>
             <h2> About page </h2>
             <ul>
+                { listItems } 
 
-
+            {/*
+                { title}
                 <li> { title } </li>
-
+            */}
             { /* <li> {title} </li> */ }
 
             { /*   { title.map(title => <> key={title.id} title={title.title} </>) }   */ }
